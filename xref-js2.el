b4bd3b992220a9f8c38b313e4fbf4eeddc07176a
Nicolas Petton <nicolas@petton.fr>
2016-04-20 18:55:26 +0200

    Better regexp for definitions

--- xref-js2.el
+++ xref-js2.el
@@ -64,7 +64,7 @@
 
 
 (defcustom xref-js2-definitions-regexps '("\\b%s\\b[\\s]*[:=][^=]"
-                                          "function[\\s]\\b%s\\b")
+                                          "function[\\s]+\\b%s\\b")
   "List of regular expressions that match definitions of a symbol.
 In each regexp string, '%s' is expanded with the searched symbol."
   :type 'list
