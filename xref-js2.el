842744114d7c8ae7a565a9ae6352d9e53aa11017
TatriX <tatrics@gmail.com>
2017-05-26 22:52:18 +0300

    Add "class" and "not new" to a definitions regex

--- xref-js2.el
+++ xref-js2.el
@@ -66,7 +66,8 @@
 
 (defcustom xref-js2-definitions-regexps '("\\b%s\\b[\\s]*[:=][^=]"
                                           "function[\\s]+\\b%s\\b"
-                                          "[^.]%s[\\s]*\\(")
+                                          "class[\\s]+\\b%s\\b"
+                                          "(?<!new)[^.]%s[\\s]*\\(")
   "List of regular expressions that match definitions of a symbol.
 In each regexp string, '%s' is expanded with the searched symbol."
   :type 'list
